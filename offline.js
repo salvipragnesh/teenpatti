260f7116f6ba99975d0842b73ec11c8a233a8ad9
salvipragnesh <61496853+salvipragnesh@users.noreply.github.com>
2021-07-19 15:24:43 +0530

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,18 +1,16 @@
 ﻿{
-	"version": 1626438862,
+	"version": 1626688444,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/sprite-sheet0.png",
-		"images/sprite2-sheet0.png",
-		"images/sprite3-sheet0.png",
-		"channel.html",
-		"c2mp-net.js",
-		"c2mp-peer.js",
-		"c2mp.js",
-		"waker.js",
-		"Photon-Javascript_SDK.min.js"
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"loading-logo.png",
+		"icon-256.png"
 	]
 }
